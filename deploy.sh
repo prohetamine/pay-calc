066cebf632eb9c868dd37d0e442650862a6b41dc
Stas Prohetamine <prohetamine@gmail.com>
2026-01-12 00:56:47 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-git add .    
-git commit -m 'update'
-git push
 set -e
 npm run build
 cp dist/index.html dist/404.html
@@ -9,5 +6,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:prohetamine/oninet-front.git master:gh-pages
+git push -f git@github.com:prohetamine/pay-calc.git master:gh-pages
 cd -
